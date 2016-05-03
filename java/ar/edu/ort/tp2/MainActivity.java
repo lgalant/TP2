ff3af740d7cad54c7b10ad421e8f20e5a862f94e
LeandroG <me@example.com>
2016-05-03 13:27:54 -0300

    initial commit

--- java/ar/edu/ort/tp2/MainActivity.java
+++ java/ar/edu/ort/tp2/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         descripcionET = (EditText) findViewById(R.id.descripcion);
